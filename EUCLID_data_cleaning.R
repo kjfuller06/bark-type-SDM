ac176dc910027095f3cb4e49e872512719012593
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-07-03 15:07:46 +1000

    Concatenated sequential splits

--- EUCLID_data_cleaning.R
+++ EUCLID_data_cleaning.R
@@ -16,10 +16,22 @@ a = c()
 b = 1
 for(i in c(2:nrow(euks))){
   if(startsWith(euks[i,], "'E") == FALSE){
+    if(startsWith(euks[i-1,], "'E") == FALSE){
+      if(startsWith(euks[i-2,], "'E") == FALSE){
+        if(startsWith(euks[i-3,], "'E") == FALSE){
+          euks[i-4,] = paste(euks[i-4,], euks[i-3,], euks[i-2,], euks[i-1,], euks[i,])
+          a[b] = i
+        }
+        euks[i-3,] = paste(euks[i-3,], euks[i-2,], euks[i-1,], euks[i,])
+        a[b] = i
+      }
+      euks[i-2,] = paste(euks[i-2,], euks[i-1,], euks[i,])
+      a[b] = i
+    }
     euks[i-1,] = paste(euks[i-1,], euks[i,])
     a[b] = i
     b = b + 1
   }
 }
 
-
+euks = euks[-a,, drop = FALSE]
