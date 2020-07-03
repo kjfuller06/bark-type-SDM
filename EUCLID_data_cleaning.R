87318977632329f61ecda975721d421e22c94c34
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-07-03 12:46:31 +1000

    Read in the dataset the best I could

--- EUCLID_data_cleaning.R
+++ EUCLID_data_cleaning.R
@@ -1,3 +1,6 @@
 # assign library path
 .libPaths("C:/Users/90946112/R/win-library/3.6.2")
 library(tidyverse)
+
+# load dataset
+flora <- read.delim("EUCLID_FriPy_webscrape2.0.txt", header = FALSE, sep = ".", col.names = "Output", strip.white = TRUE)
