9835233eee72471a6ab44fd430c1eefa8fe299bf
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-07-03 15:50:51 +1000

    Split strings to extract eucalypt species names

--- EUCLID_data_cleaning.R
+++ EUCLID_data_cleaning.R
@@ -9,9 +9,7 @@ euks <- read.delim("EUCLID_FriPy_webscrape2.0.txt", header = FALSE, sep = ".", c
 # Remove incomplete final row and NAs
 euks = na.omit(euks)
 
-backup = euks
-
-# Concatenate rows describing the same species. Some were split because they contained ".", which was used as the separator in read.delim().
+# Concatenate rows describing the same species. Some were split because they contained ".", which was used as the separator in read.delim(). The nested if statements are there to scoop up sequential rows of split strings
 a = c()
 b = 1
 for(i in c(2:nrow(euks))){
@@ -21,17 +19,27 @@ for(i in c(2:nrow(euks))){
         if(startsWith(euks[i-3,], "'E") == FALSE){
           euks[i-4,] = paste(euks[i-4,], euks[i-3,], euks[i-2,], euks[i-1,], euks[i,])
           a[b] = i
+        } else {
+          euks[i-3,] = paste(euks[i-3,], euks[i-2,], euks[i-1,], euks[i,])
         }
-        euks[i-3,] = paste(euks[i-3,], euks[i-2,], euks[i-1,], euks[i,])
-        a[b] = i
+      } else {
+        euks[i-2,] = paste(euks[i-2,], euks[i-1,], euks[i,])
       }
-      euks[i-2,] = paste(euks[i-2,], euks[i-1,], euks[i,])
-      a[b] = i
+    } else {
+      euks[i-1,] = paste(euks[i-1,], euks[i,])
     }
-    euks[i-1,] = paste(euks[i-1,], euks[i,])
     a[b] = i
     b = b + 1
   }
 }
 
 euks = euks[-a,, drop = FALSE]
+
+backup = euks
+
+euks2 = data.frame(species = 1:nrow(euks), bark = 1:nrow(euks))
+euks2[,1] = sapply(strsplit(euks$eucalypts, split=",b", fixed=TRUE), `[`, 1)
+euks2[,2] = sapply(strsplit(euks$eucalypts, split=",b", fixed=TRUE), `[`, 2)
+
+# euks2$barksplit = sapply(strsplit(euks2[,2], split="<a", fixed=TRUE), `[`)
+
