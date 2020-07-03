d3c9d853ed3847649cc55c53515c2e227f144a64
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-07-03 14:47:17 +1000

    Concatenate related strings and make list of undesirable values.

--- EUCLID_data_cleaning.R
+++ EUCLID_data_cleaning.R
@@ -1,6 +1,25 @@
-# assign library path
+# Assign library path
 .libPaths("C:/Users/90946112/R/win-library/3.6.2")
 library(tidyverse)
+library(dplyr)
 
-# load dataset
-flora <- read.delim("EUCLID_FriPy_webscrape2.0.txt", header = FALSE, sep = ".", col.names = "Output", strip.white = TRUE)
+# Load dataset
+euks <- read.delim("EUCLID_FriPy_webscrape2.0.txt", header = FALSE, sep = ".", col.names = "eucalypts", strip.white = TRUE, colClasses = "character", na.strings=c("", "'", "NA", " "))
+
+# Remove incomplete final row and NAs
+euks = na.omit(euks)
+
+backup = euks
+
+# Concatenate rows describing the same species. Some were split because they contained ".", which was used as the separator in read.delim().
+a = c()
+b = 1
+for(i in c(2:nrow(euks))){
+  if(startsWith(euks[i,], "'E") == FALSE){
+    euks[i-1,] = paste(euks[i-1,], euks[i,])
+    a[b] = i
+    b = b + 1
+  }
+}
+
+
