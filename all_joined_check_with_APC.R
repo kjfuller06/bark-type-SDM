6a1ca50b59cb5fc668bb931de25715c31dbac056
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-07-31 16:26:35 +1000

    Removed unnecessary column

--- all_joined_check_with_APC.R
+++ all_joined_check_with_APC.R
@@ -2,24 +2,22 @@ library(tidyverse)
 library(dplyr)
 
 # Load datasets
-EUCLID = read.csv("APC_name_check_EUCLID.csv", colClasses = c("NULL", rep("character", 7), rep("NULL", 16)))
-BioNet = read.csv("APC_name_check_BioNet.csv", colClasses = c("NULL", rep("character", 7), rep("NULL", 16)))
+EUCLID = read.csv("APC_name_check_EUCLID.csv", colClasses = c("NULL", rep("character", 5), "NULL", "character", rep("NULL", 16)))
+BioNet = read.csv("APC_name_check_BioNet.csv", colClasses = c("NULL", rep("character", 5), "NULL", "character", rep("NULL", 16)))
 All = read.csv("EUCLID_manual_BioNet_joinV.3_checking-1.csv", colClasses = c("NULL", rep("character", 6)))
 
 # Join APC search with BioNet and EUCLID accepted names
 All = left_join(All, BioNet, by = c("BioNet_assigned" = "Search.term"))
 All = left_join(All, EUCLID, by = c("EUCLID_checked" = "Search.term"))
-names(All)[7:18] = c("Census.BioNet",
+names(All)[7:16] = c("Census.BioNet",
                      "Matched.name.BioNet",
                      "Name.status.BioNet",
                      "Name.type.BioNet",
-                     "Tags.BioNet",
                      "canonicalName.EUCLID",
                      "Census.EUCLID",
                      "Matched.name.EUCLID",
                      "Name.status.EUCLID",
                      "Name.type.EUCLID",
-                     "Tags.EUCLID",
                      "canonicalName.EUCLID")
 
 # write csv
