fb9b70aa08d86d4d9a495bc93a54f7685da64c25
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-07-31 16:20:03 +1000

    Completed join of APC data to check accepted names

--- all_joined_check_with_APC.R
+++ all_joined_check_with_APC.R
@@ -6,7 +6,21 @@ EUCLID = read.csv("APC_name_check_EUCLID.csv", colClasses = c("NULL", rep("chara
 BioNet = read.csv("APC_name_check_BioNet.csv", colClasses = c("NULL", rep("character", 7), rep("NULL", 16)))
 All = read.csv("EUCLID_manual_BioNet_joinV.3_checking-1.csv", colClasses = c("NULL", rep("character", 6)))
 
-backup = All
-# Join EUCLID data before joining with BioNet
+# Join APC search with BioNet and EUCLID accepted names
 All = left_join(All, BioNet, by = c("BioNet_assigned" = "Search.term"))
+All = left_join(All, EUCLID, by = c("EUCLID_checked" = "Search.term"))
+names(All)[7:18] = c("Census.BioNet",
+                     "Matched.name.BioNet",
+                     "Name.status.BioNet",
+                     "Name.type.BioNet",
+                     "Tags.BioNet",
+                     "canonicalName.EUCLID",
+                     "Census.EUCLID",
+                     "Matched.name.EUCLID",
+                     "Name.status.EUCLID",
+                     "Name.type.EUCLID",
+                     "Tags.EUCLID",
+                     "canonicalName.EUCLID")
 
+# write csv
+write.csv(All, "EUCLID_manual_BioNet_joinV.4_checking-1.csv")
