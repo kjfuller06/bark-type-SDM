6740904832ef76614d3fd0f912702a0e1c905ef5
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-08-25 20:55:57 -0400

    Refined APC join

--- all_joined_check_with_APC.R
+++ all_joined_check_with_APC.R
@@ -2,22 +2,37 @@ library(tidyverse)
 library(dplyr)
 
 # Load datasets
-EUCLID = read.csv("APC_name_check_EUCLID.csv", colClasses = c("NULL", rep("character", 5), "NULL", "character", rep("NULL", 16)))
-BioNet = read.csv("APC_name_check_BioNet.csv", colClasses = c("NULL", rep("character", 5), "NULL", "character", rep("NULL", 16)))
-All = read.csv("EUCLID_manual_BioNet_joinV.3_checking-1.csv", colClasses = c("NULL", rep("character", 6)))
+# APC name search using EUCLID_checked names
+EUCLID = read.csv("APC_name_check_EUCLID.csv", colClasses = c("NULL", rep("factor", 2), rep("NULL", 21)))
+# APC name search using BioNet_assigned names
+BioNet = read.csv("APC_name_check_BioNet.csv", colClasses = c("NULL", "factor", "NULL", "factor", rep("NULL", 21)))
+# File of naming agreements between BioNet and EUCLID (manual web search)
+All = read.csv("EUCLID_manual_BioNet_joinV.3_checking-1.csv", colClasses = c("NULL", rep("factor", 4), rep("NULL", 2)))
+
+# Allow NA to be a factor level to allow for filtering
+All$EUCLID_checked = addNA(All$EUCLID_checked)
+
+# Remove all records that do not occur in NSW
 All = All %>% 
-  filter(EUCLID_checked != "Absent in NSW")
+  dplyr::filter(EUCLID_checked != "Absent in NSW" & EUCLID_checked != "Match found")
 
 # Join APC search with BioNet and EUCLID accepted names
 All = full_join(BioNet, All, by = c("Search.term" = "BioNet_assigned"))
 All = full_join(All, EUCLID, by = c("EUCLID_checked" = "Search.term"))
 All = All %>% 
-  dplyr::select(-"Census.x", -"Census.y", -"secondHybridParentNameID", -"Modification.type", -"Notes", -"Name.status")
-names(All)[c(4, 5, 9, 10)] = c("Matched.name.BioNet",
-                              "Name.type.BioNet",
-                              "Matched.name.EUCLID",
-                              "Name.type.EUCLID")
-All = unique(All)
+  dplyr::select("Confirmed_name", "Search.term", "BioNet_unassigned", "EUCLID_checked", "EUCLID_no_match")
+names(All)[1:2] = c("APC_confirmed_name", "BioNet_assigned")
+
+# Allow NA to be a factor level to allow for filtering
+All$APC_confirmed_name = addNA(All$APC_confirmed_name)
+All$BioNet_assigned = addNA(All$BioNet_assigned)
+
+# Remove 
+All = All %>%
+  dplyr::filter(APC_confirmed_name != "Excluded from APC" &
+                  BioNet_assigned != "Angophora spp." &
+                  BioNet_assigned != "Corymbia spp." &
+                  BioNet_assigned != "Eucalyptus spp.")
 
 # write csv
-write.csv(All, "EUCLID_manual_BioNet_joinV.4_checking-1.csv")
+write.csv(All, "EUCLID_manual_BioNet_joinV.4_checking-1.csv", row.names = FALSE)
