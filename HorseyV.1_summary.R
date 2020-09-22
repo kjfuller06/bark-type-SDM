2a1bf5f22bf491d33a2cf73daed4bb81d99b7d6a
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-09-22 18:57:23 -0500

    Issues with cropping need to be fixed

--- HorseyV.1_summary.R
+++ HorseyV.1_summary.R
@@ -3,13 +3,36 @@ library(tidyverse)
 library(sf)
 library(rnaturalearth)
 
+# load dataset and map of Australia
 records = st_read("data/Horsey_sampleV.1.shp")
 aus = st_read("data/australia.shp")
 
-ggplot(records)+
-  geom_sf(data = aus)+
-  geom_sf()
+# plot
+# ggplot(records)+
+#   geom_sf(data = aus)+
+#   geom_sf(aes(colour = Assg_SN))+
+#   theme(legend.position = "none")
 
-# r2.5 = getData('worldclim', var = 'bio', res = 2.5)
+# get WorldClim data
+r2.5 = getData('worldclim', var = 'bio', res = 2.5, path = "data/")
 
-# next step will be to extract environmental data and plot species + bark types against variables
+
+# This is an example from the R course. It doesn't work ####
+# reduce data size by cropping raster by extent of NSW
+# nsw = st_read("data/NSW_sans_islands.shp")
+# # create polygon of bbox of nsw
+# bb = c(140, -38, 154, -28)
+# names(bb) = c("xmin", "ymin", "xmax", "ymax")
+# attr(bb, "class") = "bbox"
+# bb <- bb %>% 
+#   st_set_crs(4326)
+# 
+# # use TA bbox to crop the raster image
+# crop(x = r2.5, y = nsw) %>% 
+#   plot
+
+
+
+# Actual next step ####
+# Getting env. data for presence records is good, then I'll be able to do variance partitioning/PCA to describe the environmental envelops of the species/trait groups
+# After that, I'll want to extract presence AND absence data for each species. This will require retaining all unique survey records from the BioNet dataset, instead of just records for the target species. Then, sampling each location for occurrence of each target species.
