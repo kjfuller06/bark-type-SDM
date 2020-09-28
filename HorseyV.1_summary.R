28d1b6008d7a9b446aa5d3f014ae43c56fdf2a47
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-09-28 13:02:41 -0500

    Added aridity dataset to environmental layers

--- HorseyV.1_summary.R
+++ HorseyV.1_summary.R
@@ -4,6 +4,7 @@ library(sf)
 library(rnaturalearth)
 
 # load dataset and map of Australia
+## need to select only columns of interest from "records". Some Aus columns are in there from the join in the previous script. I'm going back to look at all the processing steps to double-check everything.
 records = st_read("data/Horsey_sampleV.1.shp")
 aus = st_read("data/australia.shp")
 
@@ -15,7 +16,6 @@ aus = st_read("data/australia.shp")
 
 # get WorldClim data
 r2.5 = getData('worldclim', var = 'bio', res = 2.5, path = "data/")
-
 # reduce data size by cropping raster by extent of NSW
 nsw = st_read("data/NSW_sans_islands.shp") %>% 
   st_set_crs(4326)
@@ -23,7 +23,16 @@ nsw = st_read("data/NSW_sans_islands.shp") %>%
 r2.5_crop = crop(x = r2.5, y = nsw)
 # plot(r2.5_crop[['bio1']])
 
+# perform same processing for aridity data. Data were downloading by following this link: https://cgiarcsi.community/2019/01/24/global-aridity-index-and-potential-evapotranspiration-climate-database-v2/
+arid = raster('data/ai_et0/ai_et0.tif')
+# use nsw bbox to crop the raster image
+arid_crop = crop(x = arid, y = nsw)
+plot(arid_crop)
+
 # Actual next step ####
 # Getting env. data for presence records is good, then I'll be able to do variance partitioning/PCA to describe the environmental envelops of the species/trait groups
 # After that, I'll want to extract presence AND absence data for each species. This will require retaining all unique survey records from the BioNet dataset, instead of just records for the target species. Then, sampling each location for occurrence of each target species.
+
+# WorldClim data notes are in my metadata file
+
 
