ef8ef57c8157033c785d83895efd30a8487a7e47
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-09-29 17:11:00 -0500

    First attempt incomplete. Notes added for next attempt.

--- spp_selection_summary.R
+++ spp_selection_summary.R
@@ -4,41 +4,11 @@
 records = st_read("data/HorseyV.2_extracted_dataV.1.shp")
 st_geometry(records) = NULL
 
-# rename WorldClim variables by description
-# names(records)[9:27] = c(
-#   "Annual Mean Temperature",
-#   "Mean Diurnal Range (Mean of monthly (max temp - min temp))",
-#   "Isothermality (BIO2/BIO7) (×100)",
-#   "Temperature Seasonality (standard deviation ×100)",
-#   "Max Temperature of Warmest Month",
-#   "Min Temperature of Coldest Month",
-#   "Temperature Annual Range (BIO5-BIO6)",
-#   "Mean Temperature of Wettest Quarter",
-#   "Mean Temperature of Driest Quarter",
-#   "Mean Temperature of Warmest Quarter",
-#   "Mean Temperature of Coldest Quarter",
-#   "Annual Precipitation",
-#   "Precipitation of Wettest Month",
-#   "Precipitation of Driest Month",
-#   "Precipitation Seasonality (Coefficient of Variation)",
-#   "Precipitation of Wettest Quarter",
-#   "Precipitation of Driest Quarter",
-#   "Precipitation of Warmest Quarter",
-#   "Precipitation of Coldest Quarter"
-# )
-# ^ this was a terrible idea
-
 # do some environmental plotting
 r = records$ribbons
 b1 = records$bark1
 b2 = records$bark2
 
-# simplest metrics first
-b1.12 = function(x) {
-  ggplot(records, aes(bio1, bio12, color = x)) + 
-    geom_point()+
-    xlab("Annual Mean Temperature")+
-    ylab("Annual Precipitation")}
-b1.12(r)
-b1.12(b1)
-b1.12(b2)
+# visualisations:
+#   1) proportion of points occurring along an environmental gradient (as in, the proportion of temp observations that occur at each temperature), coloured and shaded by group- these could be curves for single variables and hexbins for two variables
+#   2) could also do that precip by temp visualisation Jeff sent me for the oaks project- with the overall distribution of all species in gray and individual species highlighted in red
