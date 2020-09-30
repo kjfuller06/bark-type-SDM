b397c16620920fadd0fca56554595b0c9e872a4e
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-09-30 11:38:22 -0500

    drop NANs from df

--- spp_selection_summary.R
+++ spp_selection_summary.R
@@ -1,8 +1,15 @@
 # script for visualising species selections
+library(raster)
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+library(ggplot2)
+library(RColorBrewer)
 
 # read records in again for plotting
 records = st_read("data/HorseyV.2_extracted_dataV.1.shp")
 st_geometry(records) = NULL
+records = drop_na(records)
 
 # do some environmental plotting
 r = records$ribbons
