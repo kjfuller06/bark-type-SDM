f8c6e73827eb5a78c9a17876a3b0133cb46f5817
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-05 21:40:48 -0500

    Generated presence-absence records for subsample

--- extract_P-A.R
+++ extract_P-A.R
@@ -7,4 +7,24 @@ library(ggplot2)
 library(RColorBrewer)
 
 # read records in again for plotting
-records = st_read("data/HorseyV.2_extracted_dataV.1.shp")
+records = st_read("data/Horsey_sampleV.2.shp") %>% 
+  dplyr::select(
+    "spp_shr",
+    "geometry")
+
+# write columns for each species of interest
+spp = unique(records$spp_shr)
+
+sample = records[c(1:20),]
+
+for(i in c(1:98)){
+  sample[, i+2] = NA
+  names(sample)[i+2] = spp[i]
+  for(k in c(1:nrow(sample))){
+    if(sample[k,][[1]] == names(sample)[i+2]){
+      sample[k,][[i+2]] = 1
+    } else {
+      sample[k,][[i+2]] = 0
+    }
+  }
+}
