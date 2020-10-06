ae036e0597f143153771f6811909823899f367ba
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-05 21:12:13 -0500

    Initial commit

---
+++ extract_P-A.R
@@ -0,0 +1,10 @@
+# script for visualising species selections
+library(raster)
+library(tidyverse)
+library(sf)
+library(rnaturalearth)
+library(ggplot2)
+library(RColorBrewer)
+
+# read records in again for plotting
+records = st_read("data/HorseyV.2_extracted_dataV.1.shp")
