9850288d79f885c7ce68dbbfa9e5deb261e44414
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-05 22:06:21 -0500

    Corrected the exclusion of rare species to be for <1% of occurrences, rather than <3%

--- extract_P-A.R
+++ extract_P-A.R
@@ -15,8 +15,10 @@ records = st_read("data/Horsey_sampleV.2.shp") %>%
 # write columns for each species of interest
 spp = unique(records$spp_shr)
 
+# let's work with a subsample for now
 sample = records[c(1:20),]
 
+# write a column for every unique species in the dataset and then populate the columns with presence and absence observations represented as 1 and 0, respectively
 for(i in c(1:98)){
   sample[, i+2] = NA
   names(sample)[i+2] = spp[i]
@@ -29,11 +31,13 @@ for(i in c(1:98)){
   }
 }
 
+# simplify dataframe
 st_geometry(sample) = NULL
 sample = sample %>% 
   dplyr::select(-spp_shr)
 
+# remove species that are in less than 1% of samples and greater than 50% of samples
 sumstats = colSums(sample)/nrow(sample)
-sumstats <- sumstats[which(sumstats > 0.03 & sumstats < 0.5)]
+sumstats <- sumstats[which(sumstats > 0.01 & sumstats < 0.5)]
 sample = sample[,which(names(sample) %in% names(sumstats))]
 
