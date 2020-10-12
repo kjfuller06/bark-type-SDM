e0f9958177e3bc59a84a6f190df7cf66b22a8b9d
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-12 13:24:37 -0500

    Look at individual species distributions

--- scripts/descriptive stats/check_spp_distributions.R
+++ scripts/descriptive stats/check_spp_distributions.R
@@ -4,6 +4,9 @@ library(sf)
 library(rnaturalearth)
 library(ggplot2)
 library(RColorBrewer)
+library(tmap)
 
 ## load dataset
 records = st_read("data/spp_selection_forLDA.shp")
+tmap_mode("view")
+qtm(records[records$spp_shr == "E.agglomerata",])
