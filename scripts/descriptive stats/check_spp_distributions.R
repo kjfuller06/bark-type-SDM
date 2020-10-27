31705ff4dfe061b6d7ed4c5a591736c802973839
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-10-27 14:37:50 -0500

    Checking out group distributions

--- scripts/descriptive stats/check_spp_distributions.R
+++ scripts/descriptive stats/check_spp_distributions.R
@@ -10,3 +10,26 @@ library(tmap)
 records = st_read("data/spp_selection_forLDA.shp")
 tmap_mode("view")
 qtm(records[records$spp_shr == "E.agglomerata",])
+
+# read records in again for plotting
+records = st_read("data/HorseyV.2_extracted_dataV.1.shp")
+tmap_mode("view")
+tm_shape(records[records$bark1 == "smooth",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "subfibrous - box",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "subfibrous - tessellated",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "subfibrous - rough",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "stringybark",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "smooth - short stocking",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "half bark",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "ironbark",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "subfibrous - stringy",])+tm_dots(col = "spp_shr")
+
+tm_shape(records[records$bark1 == "subfibrous - peppermint",])+tm_dots(col = "spp_shr")
