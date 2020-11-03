df5fd05b41156cdc939d0cf66292e0704373d090
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-11-03 11:53:28 -0600

    Identified EPSG for fuel layer

--- scripts/descriptive stats/layer_check.R
+++ scripts/descriptive stats/layer_check.R
@@ -2,8 +2,19 @@
 library(raster)
 library(tidyverse)
 library(sf)
+library(rgdal)
+
+# get codes
+codes = make_EPSG()
 
 # load vegetation data
 veg = raster("data/FuelTypeV2_FuelLUT1.tif")
+# look for crs code in "codes"
+lcc = codes[grep("proj=lcc", codes$prj4),]
+lcc = lcc[grep("ellps=GRS80", lcc$prj4),]
+lcc = lcc[grep("+lat_0=-33.25", lcc$prj4),]
+# EPSG is either 3308 for GDA94 / NSW Lambert or 8058 for GDA2020 / NSW Lambert
+## use 3308 as this is a government data source so likely isn't up to date
+
 # have to figure out the crs for this file
 records = st_read("data/spp_selection_forLDA.shp")
