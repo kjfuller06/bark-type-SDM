78fdc42bdd2829fa8c327ad441c74e9331f34004
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-11-10 14:05:02 -0600

    Added notes and started on reprojecting. Need to use aggregate first.

--- scripts/reproject_rasters.R
+++ scripts/reproject_rasters.R
@@ -1,48 +1,39 @@
+# this script is for reprojecting rasters to all be the same EPSG for modeling
+# 1) load datasets
+# 2) reproject NSW shapefile to each of the raster layers' EPSG and clip to make reprojection faster
+# 3) resample rasters to crs of veg layer
+#     - method = biliear for continuous data; weighted average of the four nearest cells
+#     - method = ngb for categorical data; value of the nearest cell
+
 library(raster)
+library(sf)
+library(tmap)
 
-# load the reference raster
-prsm_precip = raster("data/PRISM data/ppt/PRISM_ppt_30yr_normal_4kmM2_annual_asc.asc")
+# 1) ####
+# fuel layer
+veg = raster("data/FuelTypeV2_FuelLUT1.tif")
+# WorldClim datasets
+r2.5 = getData('worldclim', var = 'bio', res = 2.5, path = "data/")
+# aridity data from CGIARCS
+arid = raster('data/ai_et0/ai_et0.tif')
+# NSW boundary
+nsw = st_read("data/NSW_sans_islands.shp")
 
-# # reproject CSGIARCSI and NPN datasets
-# aridity = raster('data/CGIARCSI data/ai_et0/ai_et0.tif')
-# PET = raster('data/CGIARCSI data/et0_yr/et0_yr.tif')
-# agdd = raster('data/NPN data/data.tif')
-# 
-# # crop these to match the prism datasets
-# aridity = crop(aridity, extent(prsm_precip))
-# PET = crop(PET, extent(prsm_precip))
-# agdd = crop(agdd, extent(prsm_precip))
-# 
-# # reproject CSGIARCSI datasets
-# aridity = projectRaster(aridity, crs = "+proj=longlat +datum=NAD83 +no_defs", method = 'bilinear')
-# PET = projectRaster(PET, crs = "+proj=longlat +datum=NAD83 +no_defs", method = 'bilinear')
-# 
-# # write these to disk so I don't have to do it again
-# writeRaster(aridity, 'data/CGIARCSI data/ai_et0/at_et0_reprojected.tif', overwrite = TRUE)
-# writeRaster(PET, 'data/CGIARCSI data/et0_yr/et0_yr_reprojected.tif', overwrite = TRUE)
+# 2) ####
+# veg layer doesn't need cropping because its extent is already equal to NSW
 
-# reload datasets
-aridity = raster('data/CGIARCSI data/ai_et0/at_et0_reprojected.tif')
-PET = raster('data/CGIARCSI data/et0_yr/et0_yr_reprojected.tif')
+# WorldClim data
+nsw1 = nsw %>% 
+  st_transform(crs = st_crs(r2.5))
+r2.5.1 = crop(r2.5, extent(nsw1))
 
-# resample to same extent and dimensions
-aridity = aggregate(aridity, fact = 5)
-PET = aggregate(PET, fact = 5)
-aridity <- resample(aridity, prsm_precip, method = 'bilinear')
-PET <- resample(PET, prsm_precip, method = 'bilinear')
-agdd = resample(agdd, prsm_precip, method = 'bilinear')
+# aridity
+nsw1 = nsw %>% 
+  st_transform(crs = st_crs(arid))
+arid1 = crop(arid, extent(nsw1))
 
-# write to disk
-writeRaster(aridity, 'data/CGIARCSI data/ai_et0/at_et0_cropped.tif', overwrite = TRUE)
-writeRaster(PET, 'data/CGIARCSI data/et0_yr/et0_yr_cropped.tif', overwrite = TRUE)
-writeRaster(agdd, 'data/NPN data/agdd_reprojected.tif', overwrite = TRUE)
-
-# same process for monthly PET
-for(i in sprintf('%0.2d', 1:12)){
-  PET1 = raster(paste('data/CGIARCSI data/et0_month/et0_', i, '.tif', sep = ""))
-  PET1 = crop(PET1, extent(prsm_precip))
-  PET1 = projectRaster(PET1, crs = "+proj=longlat +datum=NAD83 +no_defs", method = 'bilinear')
-  PET1 = aggregate(PET1, fact = 5)
-  PET1 <- resample(PET1, prsm_precip, method = 'bilinear')
-  writeRaster(PET1, paste('data/CGIARCSI data/et0_month/et0_', i, '_projected.tif', sep = ""), overwrite = TRUE)
-}
+# 3) ####
+# aridity layer is continuous; method = 'bilinear'
+arid2 = resample(arid1, veg, method = 'bilinear')
+# WorldClim layers
+r2.5.2 = projectRaster(r2.5.1, crs = crs(veg), extent = extent(veg))
