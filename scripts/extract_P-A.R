4b9afece1556d770c2f68fc12b0bbd05848ebb53
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-11-13 13:05:45 -0600

    Generated presence/absence stats for all locations for selected species

--- scripts/extract_P-A.R
+++ scripts/extract_P-A.R
@@ -2,20 +2,43 @@
 library(tidyverse)
 library(sf)
 library(reshape2)
+library(raster)
 
 # read records in again for plotting
 records = read.csv("data/BioNet_allfloralsurvey_cleaned2.csv") %>% 
   dplyr::select(-X, -DateFirst, -DateLast)
-names(records)[3:4] = c("lat", "lon")
 spp = read.csv("data/Horsey_candidate_speciesV.4.csv")
 
+# load vegetation map
+veg = raster("data/fuels_reproj.tif")
+
+# transform records to same crs as veg layer and extract veg types
+records = records %>% 
+  st_as_sf(coords = c("Longitude_GDA94", "Latitude_GDA94"), crs = 4326) %>%
+  st_transform(crs = st_crs(veg))
+records = cbind(records, fuels_reproj = raster::extract(veg, st_coordinates(records), methods = 'simple'))
+
+# remove wetlands, grasslands, alpine habitat, urban, etc.
+types = c(42:46, 52:74)
+records = records %>% 
+  filter(!(fuels_reproj %in% types))
+
 # fill species columns with number of occurrences per unique combination of lon + lat
-df2 = dcast(df, lon + lat ~ spp_shr, fill = 0, value.var = "spp_shr")
+# remove geometry again
+records$lon = st_coordinates(records)[,1]
+records$lat = st_coordinates(records)[,2]
+st_geometry(records) = NULL
+# generate stats
+df2 = dcast(records, lon + lat ~ Assgn_ScientificName, fill = 0, value.var = "Assgn_ScientificName")
+# select only species from Horsey selection
+selection = match(spp$BioNetspecies, colnames(df2))
+selection = selection[is.na(selection) == FALSE]
+df2 = df2[,c(1, 2, selection)]
 # replace non-zero observations with "1"
 df2[,c(3:ncol(df2))] = df2[,c(3:ncol(df2))] %>% 
   mutate_if(is.numeric, ~1 * (. != 0))
 # create df with just bark traits to join back to df2 later
-info = unique(df[,c(1:4)])
+info = unique(spp[,c(1, 3, 4, 5, 6)])
 
 # remove species that are in less than 1% of samples and greater than 50% of samples
 df3 = df2
