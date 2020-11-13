698ec11b54f89ab0c4eba4411a4d4f677169cf20
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-11-13 14:12:21 -0600

    Created presence/absence dataset with no species excluded due to rarity/commonality; longform and wideform datasets exported. Improved file naming and df naming

--- scripts/extract_P-A.R
+++ scripts/extract_P-A.R
@@ -29,45 +29,36 @@ records$lon = st_coordinates(records)[,1]
 records$lat = st_coordinates(records)[,2]
 st_geometry(records) = NULL
 # generate stats
-df2 = dcast(records, lon + lat ~ Assgn_ScientificName, fill = 0, value.var = "Assgn_ScientificName")
+PA = dcast(records, lon + lat ~ Assgn_ScientificName, fill = 0, value.var = "Assgn_ScientificName")
 # select only species from Horsey selection
-selection = match(spp$BioNetspecies, colnames(df2))
+selection = match(spp$BioNetspecies, colnames(PA))
 selection = selection[is.na(selection) == FALSE]
-df2 = df2[,c(1, 2, selection)]
+PA = PA[,c(1, 2, selection)]
 # replace non-zero observations with "1"
-df2[,c(3:ncol(df2))] = df2[,c(3:ncol(df2))] %>% 
+PA[,c(3:ncol(PA))] = PA[,c(3:ncol(PA))] %>% 
   mutate_if(is.numeric, ~1 * (. != 0))
-# create df with just bark traits to join back to df2 later
+# create df with just bark traits to join back to PA later
 info = unique(spp[,c(1, 3, 4, 5, 6)])
 
-# remove species that are in less than 1% of samples and greater than 50% of samples
-df3 = df2
-no_coords = df2[,c(3:ncol(df2))]
-sumstats = colSums(no_coords)/nrow(no_coords)
-sumstats <- sumstats[which(sumstats > 0.01 & sumstats < 0.5)]
-sample = no_coords[,which(names(no_coords) %in% names(sumstats))]
-# create df3 for merging coordinates in LDA.r script
-df3 = df3[, which(names(df3) %in% names(sumstats))]
-## create shapefile for environmental data extraction
-# remove rare and common species
-# df2 = df2[,c(1, 2, which(names(df2) %in% names(sumstats)))]
-# melt back to long form
-df2 = melt(df2, id.vars = c("lon", "lat")) %>% 
-  filter(value == 1) %>% 
-  dplyr::select(-value)
+# melt to long form
+longf = melt(PA, id.vars = c("lon", "lat"))
 
 # add bark traits back to long form df
-df = df2 %>% 
-  left_join(info, by = c("variable" = "spp_shr")) %>% 
-  st_as_sf(coords = c(lon = "lon", lat = "lat"), crs = 4326)
-names(df)[1] = "spp_shr"
+longf = longf %>% 
+  left_join(info, by = c("variable" = "BioNetspecies")) %>% 
+  dplyr::select(-variable)
+
 # add new bark categories
 cat = read.csv("data/Horsey_candidate_speciesV.4_types.csv")
 cat = cat[,1:6]
-df5 = df %>% 
+longf = longf %>% 
   left_join(cat, by = c("bark1", "bark2", "ribbons"))
 
+# create shapefile as well
+longf_shp = longf %>% 
+  st_as_sf(coords = c(lon = "lon", lat = "lat"), crs = st_crs(veg))
+
 # write to disk
-write.csv(sample, "data/spp_selection_P-A.csv", row.names = FALSE)
-write.csv(df3, "data/spp_selection_P-A_coordinates.csv", row.names = FALSE)
-write_sf(df5, "data/spp_selection_forLDA.shp")
+write.csv(PA, "data/spp_selection_P-A.csv", row.names = FALSE)
+write.csv(longf, "data/spp_selection_P-A_coordinates.csv", row.names = FALSE)
+write_sf(longf_shp, "data/spp_selection_P-A.shp")
