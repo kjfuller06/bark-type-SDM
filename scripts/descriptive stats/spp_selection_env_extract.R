9220713316a5574943c3efeb5dfdb9dcee0efbe3
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-11-17 13:00:12 -0600

    Updated with new version numbers and with reprojected data layers

--- scripts/descriptive stats/spp_selection_env_extract.R
+++ scripts/descriptive stats/spp_selection_env_extract.R
@@ -1,4 +1,4 @@
-## need to update script to use reprojected rasters
+## will need to create extraction for absence records too
 library(raster)
 library(tidyverse)
 library(sf)
@@ -8,43 +8,32 @@ library(RColorBrewer)
 
 # Extract environmental data for species records ####
 ## load dataset
-records = st_read("data/spp_selection_P-A.shp")
+records = st_read("data/spp_selection_P-A.shp") %>% 
+  filter(value == "1")
 
-# load map of fuel types
-fuels = raster("data/fuels_reproj.tif")
-# reproject records to fuels crs
-records = records %>% 
-  st_transform(crs = 3308)
-# extract fuel types
-records = cbind(records, fueltype = raster::extract(fuels, st_coordinates(records), methods = 'simple'))
-
-# get WorldClim data
-# WorldClim metadata notes are in my data layers metadata xlsx
+# load datasets
 r2.5 = getData('worldclim', var = 'bio', res = 2.5, path = "data/")
-# reduce data size by cropping raster by extent of NSW
-nsw = st_read("data/NSW_sans_islands.shp") %>% 
-  st_set_crs(4326)
-# use nsw bbox to crop the raster image
-r2.5_crop = crop(x = r2.5, y = nsw)
-# reproject records to r2.5 crs
-records = records %>% 
-  st_transform(crs = 4326)
+veg = raster("data/fuels_reproj.tif")
+arid = raster("data/aridity_reproj.tif")
+nsw = st_read("data/NSW_sans_islands.shp")
+
+# clean WorldClim data
+nsw1 = nsw %>% 
+  st_transform(crs = st_crs(r2.5))
+r2.5.1 = crop(r2.5, extent(nsw1))
+r2.5.2 = projectRaster(r2.5.1, crs = crs(veg))
+
+# extract fuel types
+records = cbind(records, fueltype = raster::extract(veg, st_coordinates(records), methods = 'simple'))
 
 # extract WorldClim values at species occurrence locations and cbind to records df
-records = cbind(records, raster::extract(r2.5_crop, st_coordinates(records), methods = 'simple'))
+records = cbind(records, raster::extract(r2.5.2, st_coordinates(records), methods = 'simple'))
 
-# perform same processing for aridity data. Data were downloading by following this link: https://cgiarcsi.community/2019/01/24/global-aridity-index-and-potential-evapotranspiration-climate-database-v2/
-arid = raster('data/ai_et0/ai_et0.tif')
-# use nsw bbox to crop the raster image
-arid_crop = crop(x = arid, y = nsw)
-# plot(arid_crop)
 # extract aridity values at species occurrence locations and cbind to records df
-records = cbind(records, aridity = raster::extract(arid_crop, st_coordinates(records), methods = 'simple'))
-
-# can't do any prediction from here. Data are at different spatial res and extent. But this gives me data for plotting species envelopes.
+records = cbind(records, aridity = raster::extract(arid, st_coordinates(records), methods = 'simple'))
 
 # Plot environmental axes for each species ####
-tiff(file = "outputs/frequency_HorseyV.4_records_by_species.tiff", width =2200, height = 750, units = "px", res = 200)
+tiff(file = "outputs/frequency_HorseyV.4_records_by_speciesV.2.tiff", width =2200, height = 750, units = "px", res = 200)
 # counts
 ggplot(data.frame(records), aes(x=spp_shr)) +
   geom_bar()+
@@ -56,4 +45,4 @@ dev.off()
 ## some species have only 2-3 records
 
 # write shapefile to disk and convert sf to df, only because I'm so bad at this with sf's
-st_write(records, "data/HorseyV.4_extracted_dataV.3.shp", delete_layer = TRUE)
+st_write(records, "data/HorseyV.4_extracted_dataV.4.shp", delete_layer = TRUE)
