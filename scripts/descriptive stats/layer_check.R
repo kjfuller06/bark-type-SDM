cf1e922bc16b5d8e31dd1c2df6bc18bc9c2c7817
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-12-04 18:28:44 -0600

    Finished creating the fire history layer

--- scripts/descriptive stats/layer_check.R
+++ scripts/descriptive stats/layer_check.R
@@ -82,86 +82,22 @@ grids <- list.files("./data/fireyeartifs" , pattern = "*.tif$")
 #create a raster stack from the input raster files 
 r_all <- raster::stack(paste0("./data/fireyeartifs/", grids))
 
-# replace all non-NA values with 1
-r_all[is.na(r_all) == FALSE] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[2]][values(r_all[[1]]) > 1] = 1
-# r_all[[3]][values(r_all[[1]]) > 1] = 1
-# r_all[[4]][values(r_all[[1]]) > 1] = 1
-# r_all[[5]][values(r_all[[1]]) > 1] = 1
-# r_all[[6]][values(r_all[[1]]) > 1] = 1
-# r_all[[7]][values(r_all[[1]]) > 1] = 1
-# r_all[[8]][values(r_all[[1]]) > 1] = 1
-# r_all[[9]][values(r_all[[1]]) > 1] = 1
-# r_all[[10]][values(r_all[[1]]) > 1] = 1
-# r_all[[11]][values(r_all[[1]]) > 1] = 1
-# r_all[[12]][values(r_all[[1]]) > 1] = 1
-# r_all[[13]][values(r_all[[1]]) > 1] = 1
-# r_all[[14]][values(r_all[[1]]) > 1] = 1
-# r_all[[15]][values(r_all[[1]]) > 1] = 1
-# r_all[[16]][values(r_all[[1]]) > 1] = 1
-# r_all[[17]][values(r_all[[1]]) > 1] = 1
-# r_all[[18]][values(r_all[[1]]) > 1] = 1
-# r_all[[19]][values(r_all[[1]]) > 1] = 1
-# r_all[[20]][values(r_all[[1]]) > 1] = 1
-# r_all[[21]][values(r_all[[1]]) > 1] = 1
-# r_all[[22]][values(r_all[[1]]) > 1] = 1
-# r_all[[23]][values(r_all[[1]]) > 1] = 1
-# r_all[[24]][values(r_all[[1]]) > 1] = 1
-# r_all[[25]][values(r_all[[1]]) > 1] = 1
-# r_all[[26]][values(r_all[[1]]) > 1] = 1
-# r_all[[27]][values(r_all[[1]]) > 1] = 1
-# r_all[[28]][values(r_all[[1]]) > 1] = 1
-# r_all[[29]][values(r_all[[1]]) > 1] = 1
-# r_all[[30]][values(r_all[[1]]) > 1] = 1
-# r_all[[31]][values(r_all[[1]]) > 1] = 1
-# r_all[[32]][values(r_all[[1]]) > 1] = 1
-# r_all[[33]][values(r_all[[1]]) > 1] = 1
-# r_all[[34]][values(r_all[[1]]) > 1] = 1
-# r_all[[35]][values(r_all[[1]]) > 1] = 1
-# r_all[[36]][values(r_all[[1]]) > 1] = 1
-# r_all[[37]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
-# r_all[[1]][values(r_all[[1]]) > 1] = 1
+# count the number of occurrences in which each pixel is greater than 1
+firehist = sum(!is.na(r_all)) 
+
+writeRaster(firehist, "data/firehistory.tif")
+
+# get soil data ####
+library(gdalUtils)
+library(XML)
+
+# bulk density
+voi = "bdod" # variable of interest
+depth = "0-5cm"
+quantile = "mean"
+
+voi_layer = paste(voi,depth,quantile, sep="_") # layer of interest 
+
+wcs_path = paste0("https://maps.isric.org/mapserv?map=/map/",voi,".map") # Path to the WCS. See maps.isric.org
+wcs_service = "SERVICE=WCS"
+wcs_version = "VERSION=2.0.1" # This works for gdal >=2.3; "VERSION=1.1.1" works with gdal < 2.3.
