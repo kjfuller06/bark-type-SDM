a379bde6d2598fb3a1e16ac30882d9334ac634d0
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-12-09 17:16:52 -0600

    Started working on my pipeline for exporting random forest results

--- scripts/descriptive stats/randomForest.R
+++ scripts/descriptive stats/randomForest.R
@@ -6,17 +6,23 @@ library(tmap)
 library(randomForest)
 
 # testing random forest ####
-records = read.csv("data/HorseyV.4_extracted_dataV.4_P-A_allenv.csv")
+records = read.csv("data/HorseyV.4_extracted_dataV.5_P-A_allenv.csv")
 
 records = drop_na(records)
 # records$fueltype = as.factor(records$fueltype)
 ### can't use fuel type yet because it has too many categories for a categorical variable
 
 # run random forest for a selected species
-rf_sp1 = randomForest( x=records[,118:137], y=as.factor(records[,52]), ntree=1000, nodesize=10, importance =T)
+rf_sp1 = randomForest( x=records[,118:148], y=as.factor(records[,1]), ntree=1000, nodesize=10, importance =T)
+rf_sp1
 # check importance of variables
 importance(rf_sp1)
 
+df = data.frame(sp = seq(1:116), "correct0" = seq(1:116), "false1" = seq(1:116), "false0" = seq(1:116), "correct1" = seq(1:116), class.error0 = seq(1:116), class.error1 = seq(1:116))
+
+df = data.frame(sp = names(records[52]), "correct0" = rf_sp1$confusion[1, 1], "false1" = rf_sp1$confusion[1, 2], "false0" = rf_sp1$confusion[2, 1], "correct1" = rf_sp1$confusion[2, 2], class.error0 = rf_sp1$confusion[1, 3], class.error1 = rf_sp1$confusion[2, 3])
+
+write.csv(df, "outputs/randomForest_HorseyV.4_extracted_dataV.5_P-!_allenv.csv", row.names = FALSE)
 
 # let's look at a bark type
 # add all observations of the following species to create the stringybark category
@@ -45,6 +51,7 @@ records$stringy = records$Eucalyptus.agglomerata +
 records$stringy[records$stringy != 0] = 1
 
 # run random forest for a selected species
-rf_b1 = randomForest( x=records[,118:137], y=as.factor(records[,140]), ntree=1000, nodesize=10, importance =T)
+rf_b1 = randomForest( x=records[,118:148], y=as.factor(records[,151]), ntree=1000, nodesize=10, importance =T)
+rf_b1
 # check importance of variables
 importance(rf_b1)
