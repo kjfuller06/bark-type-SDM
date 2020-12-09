8c61dc0061116d9303a6f5d2d794bdf15f412474
Kathryn Fuller <90946112@westernsydney.edu.au>
2020-12-09 14:59:23 -0600

    Added new layers. Corrected units in bioclim temperature variables

--- scripts/descriptive stats/spp_selection_env_extract.R
+++ scripts/descriptive stats/spp_selection_env_extract.R
@@ -1,4 +1,3 @@
-# Need to add fire and soil data from disk and extract. Need to correct temperature units in bioclim data from C*10 to C and extract.
 library(raster)
 library(tidyverse)
 library(sf)
@@ -57,21 +56,44 @@ records = read.csv("data/spp_selection_P-A_allenv.csv") %>%
 r2.5 = getData('worldclim', var = 'bio', res = 2.5, path = "data/")
 arid = raster("data/aridity_reproj.tif")
 nsw = st_read("data/NSW_sans_islands.shp")
+fire = raster("data/fire_reproj.tif")
+bdod = raster("data/bdod_reproj.tif")
+cec = raster("data/cec_reproj.tif")
+cfvo = raster("data/cfvo_reproj.tif")
+sand = raster("data/sand_reproj.tif")
+silt = raster("data/silt_reproj.tif")
+clay = raster("data/clay_reproj.tif")
+nit = raster("data/nitrogen_reproj.tif")
+ph = raster("data/ph_reproj.tif")
+soc = raster("data/soc_reproj.tif")
+ocd = raster("data/ocd_reproj.tif")
+
+# stack soils data
+soils = raster::stack(bdod, cec, cfvo, sand, silt, clay, nit, ph, soc, ocd)
 
 # clean WorldClim data
 nsw1 = nsw %>% 
   st_transform(crs = st_crs(r2.5))
 r2.5.1 = crop(r2.5, extent(nsw1))
 r2.5.2 = projectRaster(r2.5.1, crs = crs(veg))
+temps = r2.5.2[[c('bio1', 'bio2', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9', 'bio10', 'bio11')]]/10
+bio_other = r2.5.2[[c('bio3', 'bio4', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19')]]
 
 # extract fuel types
 records = cbind(records, fueltype = raster::extract(veg, st_coordinates(records), methods = 'simple'))
 
 # extract WorldClim values at species occurrence locations and cbind to records df
-records = cbind(records, raster::extract(r2.5.2, st_coordinates(records), methods = 'simple'))
+records = cbind(records, raster::extract(temps, st_coordinates(records), methods = 'simple'))
+records = cbind(records, raster::extract(bio_other, st_coordinates(records), methods = 'simple'))
 
 # extract aridity values at species occurrence locations and cbind to records df
 records = cbind(records, aridity = raster::extract(arid, st_coordinates(records), methods = 'simple'))
+
+# extract fire history
+records = cbind(records, firehistory = raster::extract(fire, st_coordinates(records), methods = 'simple'))
+
+# extract soil data
+records = cbind(records, raster::extract(soils, st_coordinates(records), methods = 'simple'))
 
 # write shapefile to disk
 # st_write(records, "data/HorseyV.4_extracted_dataV.4_P-A_allenv.shp", delete_layer = TRUE)
