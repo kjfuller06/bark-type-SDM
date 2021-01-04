8151e25a4d0d5f504976504933ec6b546710fd0b
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-01-04 17:11:38 -0600

    First attempt at running an analysis on tiles

--- scripts/terrain_analysis.R
+++ scripts/terrain_analysis.R
@@ -5,27 +5,26 @@ library(tmap)
 library(tidyverse)
 library(insol)
 library(meteo)
+library(GSIF)
+library(rgdal)
 
-# NSW polygon
-nsw = st_read("data/NSW_sans_islands.shp")
 # DEM-H dataset
-dem = raster("data/DEM/71498/a05f7893-0050-7506-e044-00144fdd4fa6/hdr.adf")
-dem
-
-# crop DEM with NSW extent shapefile
-# reproject shapefile
-nsw = nsw %>% 
-  st_transform(crs = st_crs(dem))
-dem = crop(dem, nsw)
-tm_shape(dem)+tm_raster()+tm_shape(nsw)+tm_borders()
+dem = raster("data/DEM_nsw.tif")
 
 # calculate slope
 # dem = slope(cgrad(dem),degrees=TRUE)
 ## ^vector too large
 
 # break raster into tiles for processing
-tiles = tiling(dem,tilesize=2500,overlapping=25,aspoints=FALSE)
+obj <- GDALinfo("data/DEM_nsw.tif")
+tile.tbl = GSIF::getSpatialTiles(obj, block.x = 1, overlap.percent = 5, return.SpatialPolygons = FALSE)
+tile.tbl$ID = as.character(1:nrow(tile.tbl))
 
-# calculate slope
-demslope = slope(cgrad(tiles[[1]]),degrees=TRUE)
-demslope = raster(demslope, crs = crs(dem))
+# demslopes = slope(cgrad(dem),degrees=TRUE)
+## ^this needs an actual slice of the dem to run. tile.tbl is just an index atm
+
+# tiles = tiling(dem,tilesize=2500,overlapping=25,aspoints=FALSE)
+# 
+# # calculate slope
+# demslope = slope(cgrad(tiles[[1]]),degrees=TRUE)
+# demslope = raster(demslope, crs = crs(dem))
