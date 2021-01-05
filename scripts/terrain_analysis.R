e80bb5ced6513e71cd411fccaa2dfb16bb3cac3a
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-01-05 14:53:06 -0600

    Slope and aspect calculated just fine without tiling

--- scripts/terrain_analysis.R
+++ scripts/terrain_analysis.R
@@ -12,9 +12,14 @@ library(snowfall)
 # load DEM-H
 dem = raster("data/DEM_nsw.tif")
 
+# calculate slope and aspect
+dem_slope = terrain(dem, opt = c('slope', 'aspect'), unit = 'degrees')
+# write to disk
+writeRaster(dem_slope, "data/dem_slope.aspect_30m.grd", format = "raster", overwrite = TRUE)
+
 # break raster into tiles for processing
 obj <- GDALinfo("data/DEM_nsw.tif")
-tile.tbl = GSIF::getSpatialTiles(obj, block.x = 1, overlap.percent = 5, return.SpatialPolygons = FALSE)
+tile.tbl = GSIF::getSpatialTiles(obj, block.x = 0.1, overlap.percent = 5, return.SpatialPolygons = FALSE)
 tile.tbl$ID = as.character(1:nrow(tile.tbl))
 
 slope_function <- function(i, tile.tbl, dem, dem_path = "./data/DEM_nsw.tif", out.path="./data/DEM/tiles/"){
@@ -27,12 +32,14 @@ slope_function <- function(i, tile.tbl, dem, dem_path = "./data/DEM_nsw.tif", ou
                         silent = TRUE)
     names(newdata) <- "altitude"
     newdata = raster(newdata)
-    dem_slopes <- raster(slope(cgrad(newdata), degree = TRUE), crs = projection(newdata))
+    dem_slopes <- terrain(newdata, opt = c('slope', 'aspect'), unit = 'degrees')
     extent(dem_slopes) = extent(newdata)
     writeGDAL(dem_slopes, out.tif, drivername = "GTiff", type = "Int16", 
               options="COMPRESS=DEFLATE", copy_drivername = "GTiff")
   }
 }
+
+memory.limit(size = 56000)
 
 sfInit(parallel = TRUE, cpus = parallel::detectCores())
 sfExport("slope_function", "tile.tbl", "dem")
