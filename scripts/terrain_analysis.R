c8c813292f443ffd254d2e5df09eebe51f9972be
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-01-05 11:17:01 -0600

    Got all the pieces working in the tiling function

--- scripts/terrain_analysis.R
+++ scripts/terrain_analysis.R
@@ -20,7 +20,24 @@ obj <- GDALinfo("data/DEM_nsw.tif")
 tile.tbl = GSIF::getSpatialTiles(obj, block.x = 1, overlap.percent = 5, return.SpatialPolygons = FALSE)
 tile.tbl$ID = as.character(1:nrow(tile.tbl))
 
-# demslopes = slope(cgrad(dem),degrees=TRUE)
+slope_function <- function(m.DLSM, i, tile.tbl, dem_tif = "./data/DEM_nsw.tif", out.path="./data/DEM/tiles/"){
+  out.tif = paste0(out.path, "T_", tile.tbl[i,"ID"], ".tif")
+  if(!file.exists(out.tif)){
+    newdata <- readGDAL(dem_tif, offset=unlist(tile.tbl[i,c("offset.y","offset.x")]),
+                        region.dim=unlist(tile.tbl[i,c("region.dim.y","region.dim.x")]),
+                        output.dim=unlist(tile.tbl[i,c("region.dim.y","region.dim.x")]),
+                        silent = TRUE)
+    names(newdata) <- "altitude"
+    newdata = raster(newdata)
+    dem_slopes <- raster(slope(cgrad(newdata), degree = TRUE), crs = projection(newdata))
+    extent(dem_slopes) = extent(newdata)
+    writeRaster(dem_slopes, out.tif)
+  }
+}
+
+
+
+demslopes = slope(cgrad(dem),degrees=TRUE)
 ## ^this needs an actual slice of the dem to run. tile.tbl is just an index atm
 
 # tiles = tiling(dem,tilesize=2500,overlapping=25,aspoints=FALSE)
