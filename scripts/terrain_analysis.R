6ecc21a8d213c42da2094860ef69c1931859e09d
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-01-05 11:51:38 -0600

    All lines run. Vectors too large for R. Need to reduce size by at least 1/3

--- scripts/terrain_analysis.R
+++ scripts/terrain_analysis.R
@@ -7,23 +7,21 @@ library(insol)
 library(meteo)
 library(GSIF)
 library(rgdal)
+library(snowfall)
 
-# DEM-H dataset
+# load DEM-H
 dem = raster("data/DEM_nsw.tif")
-
-# calculate slope
-# dem = slope(cgrad(dem),degrees=TRUE)
-## ^vector too large
 
 # break raster into tiles for processing
 obj <- GDALinfo("data/DEM_nsw.tif")
 tile.tbl = GSIF::getSpatialTiles(obj, block.x = 1, overlap.percent = 5, return.SpatialPolygons = FALSE)
 tile.tbl$ID = as.character(1:nrow(tile.tbl))
 
-slope_function <- function(m.DLSM, i, tile.tbl, dem_tif = "./data/DEM_nsw.tif", out.path="./data/DEM/tiles/"){
+slope_function <- function(i, tile.tbl, dem, dem_path = "./data/DEM_nsw.tif", out.path="./data/DEM/tiles/"){
   out.tif = paste0(out.path, "T_", tile.tbl[i,"ID"], ".tif")
   if(!file.exists(out.tif)){
-    newdata <- readGDAL(dem_tif, offset=unlist(tile.tbl[i,c("offset.y","offset.x")]),
+    cov.files = paste0(dem_path, dem)
+    newdata <- readGDAL(cov.files, offset=unlist(tile.tbl[i,c("offset.y","offset.x")]),
                         region.dim=unlist(tile.tbl[i,c("region.dim.y","region.dim.x")]),
                         output.dim=unlist(tile.tbl[i,c("region.dim.y","region.dim.x")]),
                         silent = TRUE)
@@ -31,17 +29,15 @@ slope_function <- function(m.DLSM, i, tile.tbl, dem_tif = "./data/DEM_nsw.tif", 
     newdata = raster(newdata)
     dem_slopes <- raster(slope(cgrad(newdata), degree = TRUE), crs = projection(newdata))
     extent(dem_slopes) = extent(newdata)
-    writeRaster(dem_slopes, out.tif)
+    writeGDAL(dem_slopes, out.tif, drivername = "GTiff", type = "Int16", 
+              options="COMPRESS=DEFLATE", copy_drivername = "GTiff")
   }
 }
 
+sfInit(parallel = TRUE, cpus = parallel::detectCores())
+sfExport("slope_function", "tile.tbl", "dem")
+sfLibrary(rgdal)
 
-
-demslopes = slope(cgrad(dem),degrees=TRUE)
-## ^this needs an actual slice of the dem to run. tile.tbl is just an index atm
-
-# tiles = tiling(dem,tilesize=2500,overlapping=25,aspoints=FALSE)
-# 
-# # calculate slope
-# demslope = slope(cgrad(tiles[[1]]),degrees=TRUE)
-# demslope = raster(demslope, crs = crs(dem))
+out.lst <- sfClusterApplyLB(1:nrow(tile.tbl), 
+                             function(x){ slope_function(x, tile.tbl, dem) })
+sfStop()
