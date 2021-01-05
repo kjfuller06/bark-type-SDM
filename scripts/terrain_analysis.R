8461dcea309f7116ce71c8cb6348bf5095691af0
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-01-05 17:20:38 -0600

    Managed to export the slopes and aspects to file. Everything else commented out because it can't run- not enough space or memory or some other issue

--- scripts/terrain_analysis.R
+++ scripts/terrain_analysis.R
@@ -13,38 +13,46 @@ library(snowfall)
 dem = system.file("data/DEM_nsw.tif", package = "rgdal")
 
 # calculate slope and aspect
-dem_slope = terrain(dem, opt = c('slope', 'aspect', 'TPI', 'TRI', 'roughness'), unit = 'degrees')
+dem_slopes = terrain(dem, opt = c('slope', 'aspect'), unit = 'degrees')
 # write to disk
-writeRaster(dem_slope, "data/dem_slope.aspect_30m.grd", format = "raster", overwrite = TRUE)
+writeRaster(dem_slope, "data/dem_slope.aspect_30m.grd", format = "raster", options = "COMPRESS=DEFLATE", overwrite = TRUE)
+# writeGDAL(dem_slope, "data/dem_slope.aspect_30m.grd", drivername = "GTiff", type = "Int16", 
+#           options="COMPRESS=DEFLATE", copy_drivername = "GTiff")
+# system(paste0('gdalwarp',
+# dem_slope[[1]],
+# '\"./data/dem_slope_30m.tif\"',
+# '-ot \"Float32\" -co \"BIGTIFF=YES\"',
+# '-wm 2000 -co \"COMPRESS=DEFLATE\" -overwrite -multi',
+# '-wo \"NUM_THREADS=ALL_CPUS\"'))
 
 # break raster into tiles for processing
-obj <- GDALinfo(dem)
-tile.tbl = GSIF::getSpatialTiles(obj, block.x = 0.1, overlap.percent = 5, return.SpatialPolygons = FALSE)
-tile.tbl$ID = as.character(1:nrow(tile.tbl))
-
-slope_function <- function(i, tile.tbl, dem, dem_path = "./data/DEM_nsw.tif", out.path="./data/DEM/tiles/"){
-  out.tif = paste0(out.path, "T_", tile.tbl[i,"ID"], ".tif")
-  if(!file.exists(out.tif)){
-    cov.files = paste0(dem_path, dem)
-    newdata <- readGDAL(cov.files, offset=unlist(tile.tbl[i,c("offset.y","offset.x")]),
-                        region.dim=unlist(tile.tbl[i,c("region.dim.y","region.dim.x")]),
-                        output.dim=unlist(tile.tbl[i,c("region.dim.y","region.dim.x")]),
-                        silent = TRUE)
-    names(newdata) <- "altitude"
-    newdata = raster(newdata)
-    dem_slopes <- raster(slope(cgrad(newdata), degree = TRUE), crs = projection(newdata))
-    extent(dem_slopes) = extent(newdata)
-    writeGDAL(dem_slopes, out.tif, drivername = "GTiff", type = "Int16", 
-              options="COMPRESS=DEFLATE", copy_drivername = "GTiff")
-  }
-}
-
-memory.limit(size = 56000)
-
-sfInit(parallel = TRUE, cpus = parallel::detectCores())
-sfExport("slope_function", "tile.tbl", "dem")
-sfLibrary(rgdal)
-
-out.lst <- sfClusterApplyLB(1:nrow(tile.tbl), 
-                             function(x){ slope_function(x, tile.tbl, dem) })
-sfStop()
+# obj <- GDALinfo(dem)
+# tile.tbl = GSIF::getSpatialTiles(obj, block.x = 0.1, overlap.percent = 5, return.SpatialPolygons = FALSE)
+# tile.tbl$ID = as.character(1:nrow(tile.tbl))
+# 
+# slope_function <- function(i, tile.tbl, dem, dem_path = "./data/DEM_nsw.tif", out.path="./data/DEM/tiles/"){
+#   out.tif = paste0(out.path, "T_", tile.tbl[i,"ID"], ".tif")
+#   if(!file.exists(out.tif)){
+#     cov.files = paste0(dem_path, dem)
+#     newdata <- readGDAL(cov.files, offset=unlist(tile.tbl[i,c("offset.y","offset.x")]),
+#                         region.dim=unlist(tile.tbl[i,c("region.dim.y","region.dim.x")]),
+#                         output.dim=unlist(tile.tbl[i,c("region.dim.y","region.dim.x")]),
+#                         silent = TRUE)
+#     names(newdata) <- "altitude"
+#     newdata = raster(newdata)
+#     dem_slopes <- raster(slope(cgrad(newdata), degree = TRUE), crs = projection(newdata))
+#     extent(dem_slopes) = extent(newdata)
+#     writeGDAL(dem_slopes, out.tif, drivername = "GTiff", type = "Int16", 
+#               options="COMPRESS=DEFLATE", copy_drivername = "GTiff")
+#   }
+# }
+# 
+# memory.limit(size = 56000)
+# 
+# sfInit(parallel = TRUE, cpus = parallel::detectCores())
+# sfExport("slope_function", "tile.tbl", "dem")
+# sfLibrary(rgdal)
+# 
+# out.lst <- sfClusterApplyLB(1:nrow(tile.tbl), 
+#                              function(x){ slope_function(x, tile.tbl, dem) })
+# sfStop()
