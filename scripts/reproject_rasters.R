e455905f1d7bbca10e57cee15f30c117ef425f11
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-01-08 18:26:53 -0500

    Removed intermediate bioclim rasterstacks

--- scripts/reproject_rasters.R
+++ scripts/reproject_rasters.R
@@ -97,6 +97,7 @@ bioclim = projectRaster(bioclim, fire, method = 'bilinear')
 temps = bioclim[[c('bio1', 'bio2', 'bio5', 'bio6', 'bio7', 'bio8', 'bio9', 'bio10', 'bio11')]]/10
 bio_other = bioclim[[c('bio3', 'bio4', 'bio12', 'bio13', 'bio14', 'bio15', 'bio16', 'bio17', 'bio18', 'bio19')]]
 bioclim = raster::stack(temps, bio_other)
+rm(temps, bio_other)
 # write to disk and remove from memory
 writeRaster(bioclim, "data/bioclim_80m.grd", format = "raster", options = "COMPRESS=DEFLATE", overwrite = TRUE)
 rm(bioclim)
