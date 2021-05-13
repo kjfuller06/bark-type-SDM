f217e9c09199a374e0c39769ba44cbc952f5d6e7
Kathryn Fuller <90946112@westernsydney.edu.au>
2021-05-13 15:33:29 -0500

    Reworked rasterToPoints() script

--- scripts/step0_layer_processing_cheyenne.R
+++ scripts/step0_layer_processing_cheyenne.R
@@ -1597,10 +1597,10 @@ library(tmap)
 r_precip <- raster("data/ASCIIgrid.txt")
 crs(r_precip) <- '+init=EPSG:4326' # WGS84
 r_precip 
-aus = getData("GADM", country = "AUS", level = 1)
-precip = mask(r_precip, aus)
-res(precip)
-precip = aggregate(precip, fact = 10)
+aus = readRDS("gadm36_AUS_1_sp.rds")
+res(r_precip)
+precip = aggregate(r_precip, fact = 10)
+precip = mask(precip, aus)
 df = as.data.frame(rasterToPoints(precip), xy = TRUE)
 df = df %>% 
   st_as_sf(coords = c("x", "y"), crs = st_crs(precip))
@@ -1800,6 +1800,52 @@ capture.output(
 
 ## values are missing for all y's under 4638061; full NSW range extends to 4027276
 
+
+#------------------- rasterToPoints_redo ---------------------
+library(raster)
+library(sf)
+library(parallel)
+library(snowfall)
+
+captute.output(
+  paste0("libraries loaded"),
+  file = "raster_extract_notes.txt"
+)
+
+setwd("/glade/scratch/kjfuller/data")
+
+mask = list.files("./", pattern = "^mask", recursive = FALSE, full.names = TRUE)
+
+df_fun = function(x){
+  r = raster(mask[x])
+  n1 = sum(is.na(values(r)))
+  df = as.data.frame(rasterToPoints(r), xy = TRUE)
+  n2 = sum(is.na(df))
+  lab = substr(mask[x], 14, nchar(mask[x])-4)
+  write.csv(df, paste0(lab, "_forPCA.csv"), row.names = FALSE)
+  capture.output(
+    paste0("nrow of ", lab, " = ", nrow(df)),
+    paste0("NaNs in raster of ", lab, " = ", n1),
+    paste0("NaNs in df of ", lab, " = ", n2),
+    file = paste0("raster_extract_", lab, ".txt")
+  )
+}
+
+capture.output(
+  paste0("mask list and function loaded"),
+  paste0("initiating snowfall"),
+  file = "raster_extract_notes.txt",
+  append = TRUE
+)
+
+sfInit(parallel = TRUE, cpus = 36)
+sfExport("mask", "df_fun")
+sfLibrary(raster)
+sfLibrary(sf)
+
+sfLapply(c(1:length(mask)), df_fun)
+
+sfStop()
 
 #-------------------- PC values for RFs -------------------
 library(data.table)
