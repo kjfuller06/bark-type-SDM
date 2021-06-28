0bd6541e09889f669c3bb3dd3a04e4a37bd0e468
kjfuller06 <90946112@westernsydney.edu.au>
2021-06-28 15:03:35 -0500

    Updated with mallee/tree switching data

--- scripts/step1_ extract_P-A.R
+++ scripts/step1_ extract_P-A.R
@@ -10,12 +10,11 @@ library(raster)
 
 # process data ####
 # load datasets
-records = read.csv("data/BioNet_allfloralsurvey_cleaned2.csv") %>% 
-  dplyr::select(-X, -DateFirst, -DateLast)
+records = read.csv("outputs/BioNet_allfloralsurvey_cleaned2.csv") %>% 
+  dplyr::select(-DateFirst, -DateLast)
 nomen = read.csv("data/Candidate_speciesV.1.csv") %>% 
-  dplyr::select(-BioNet) %>% 
   unique()
-names(nomen) = c("Assgn_ScientificName", "NicolleName")
+names(nomen) = c("ScientificName", "Assgn_ScientificName", "NicolleName")
 traits = read_sf("data/species_sampleV.1.shp")
 veg = raster("data/fuels_30m.tif")
 
@@ -52,30 +51,31 @@ records = records %>%
 records$lon = st_coordinates(records)[,1]
 records$lat = st_coordinates(records)[,2]
 st_geometry(records) = NULL
-## 457,486 records
+## 462,150 records
 records = records %>% 
   left_join(traits)
-## this takes the data frame from nrow() = 457,486 to nrow() = 458,587, so some sites have both the tree and mallee forms of either/both E. goniocalyx and/or E. socialis
+## 464,290 records
+## this takes the data frame from nrow() = 462,150 to nrow() = 464,290, so some sites have both the tree and mallee forms of switchable species
 for(i in c(1:nrow(records))){
   if(is.na(records$Ncll19N[i]) == FALSE){
     records$Assgn_ScientificName[i] = records$Ncll19N[i]
   }
 }
-records = records[,c(1:4)]
+records = records[,c(1:4, 6:7)]
 
 # convert back into sf and extract veg types
 records = records %>% 
   st_as_sf(coords = c("lon", "lat"), crs = st_crs(veg))
 records = cbind(records, fuel = raster::extract(veg, st_coordinates(records), methods = 'simple'))
-## 458,587 records
+## 464,290 records
 
 # remove wetlands, grasslands, alpine habitat, urban, etc.
 types = c(52:74)
 records = records %>%
   filter(!(fuel %in% types))
-## 277,295 records
+## 281,026 records
 records = drop_na(records)
-## 270,641 records
+## 274,372 records
 ## dropped records lie outside the veg type boundary- sometimes these were still within the state where the veg layer was inaccurately clipped
 backup = records
 
@@ -88,14 +88,14 @@ st_geometry(records) = NULL
 # generate stats
 PA = dcast(records, lon + lat ~ Assgn_ScientificName, fill = 0, value.var = "Assgn_ScientificName")
 ## 8,326 rows = sampling locations
-## 5,101 columns = species/subspecies
+## 5,120 columns = species/subspecies
 
 # select only species from Horsey selection
 selection = match(unique(traits$Ncll19N), colnames(PA))
 selection = selection[is.na(selection) == FALSE]
 PA = PA[,c(1, 2, selection)]
 ## 8,326 rows = sampling locations
-## 181 columns = species/subspecies
+## 182 columns = species/subspecies
 
 # replace non-zero observations with "1"
 PA[,c(3:ncol(PA))] = PA[,c(3:ncol(PA))] %>% 
@@ -103,10 +103,9 @@ PA[,c(3:ncol(PA))] = PA[,c(3:ncol(PA))] %>%
 
 # melt to long form
 longf = melt(PA, id.vars = c("lon", "lat"))
-## 1,490,354 observations
-## 1,476,729 as 0's
-## 13,625 as 1's
-### mallee form addition added just 6 observations of "1" to the dataset
+## 1,498,680 observations
+## 1,485,325 as 0's
+## 13,355 as 1's
 
 # add bark traits back to long form df
 longf = longf %>% 
